4353f1724c8658fb8d1abe3ef88caef897b154de
Daniel Luiz <dancarvalho0808@gmail.com>
2026-08-18 16:30:31 -0300

    style: melhorando as: Cards, Banner e Header

--- open-pizzaria/src/App.jsx
+++ open-pizzaria/src/App.jsx
@@ -1,55 +1,21 @@
-
+import Header from './Componentes/Header';
+import Banner from './Componentes/Banner';
+import Cards from './Componentes/Cards';
+import "./Global.css";
 
 function App() {
-  
+
   return (
-    <>
-      <header>
-        <h1>🍕 Open Pizzaria</h1>
-        <p>
-          Seja bem-vindo à Open Pizzaria!<br/>
-        Sabores incríveis, ingredientes selecionados e pizzas feitas com carinho.
-        </p>
-      </header>
-      <hr />
+    <div className="container">
 
-     <section className="sobre-pizzaria">
-  <article>
-    <h2>Nossos Sabores</h2>
-    <p>
-      Descubra uma variedade de sabores preparados especialmente para você,
-      desde as pizzas tradicionais até combinações irresistíveis.
-    </p>
-  </article>
 
-  <article>
-    <h2>Feito com Carinho</h2>
-    <p>
-      Na Open Pizzaria, cada pizza é preparada com ingredientes selecionados
-      e muito carinho, para levar até você uma experiência deliciosa.
-    </p>
-  </article>
+      <Header />
+      <Banner />
+      <Cards />
 
-  <p>
-    Venha saborear a Open Pizzaria e transforme seu momento em uma experiência
-    cheia de sabor, alegria e aquela pizza quentinha que todo mundo ama! 🍕❤️
-  </p>
-</section>
+    </div>
 
-<section className="banner">
-  <img
-    src="https://images.unsplash.com/photo-1579751626657-72bc17010498?auto=format&fit=crop&w=1000&h=300&q=80"
-    alt="Pizza da Open Pizzaria"
-    width="1000"
-    height="300"
-  />
-</section>
-      
-      
-      
-      </>
 
-      
   )
 }
 
